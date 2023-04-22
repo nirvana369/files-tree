30751e2e332b50a0ffe44effc6edaae05529320c
nirvana369 <hoanglinh91@gmail.com>
2023-04-22 16:23:35 +0700

    Update Howtouse

--- frontend/components/HowToUse.svelte
+++ frontend/components/HowToUse.svelte
@@ -2,22 +2,24 @@
   <Accordion style="padding-top: 50px;">
     <AccordionItem title="Sync/create new folder">
       <p>
-        1/ Click [Create new] to pick local folder and wait a little to load and verify folder.<br><br>
-        2/ Click [Sync] button to sync folder to on-chain.<br><br>
-        When folder sync complete, you can click [Download] button to store files to local.<br><br>
-        WARN : If you not login, when you created folder, it will be public access.
+        I recommend using Stoic wallet.
+        1/ Click [Create New] to select a local folder and wait a bit for it to load and verify the folder.<br><br>
+        2/ Click the [Sync] button to synchronize the folder with on the server.<br><br>
+        When folder synchronization is complete, you can click the [Download] button to save the file locally.<br><br>
+        WARNING: If you are not logged in, when you create a folder, the folder will be publicly accessible.
       </p>
     </AccordionItem>
     <AccordionItem title="Sync/merge folder local to on-chain">
       <p>
-        1/ After login completed, folders you created/synced will loaded on your screen.<br><br>
-        2/ Choose folder and click [Sync] button, dialog will open to choose local folder you want to merge and sync.<br><br>
-        When folder sync complete, you can click [Download] button to sync all files to local.
+        1/ Once the login is complete, the folders you created/synced will be loaded on your home page.<br><br>
+        2/ Select the folder and click [Sync] button, a dialog will open to select the local folder you want to merge and sync.<br><br>
+        After selecting a folder to sync, the application will process uploading files in the selected folder locally to the server, and download files from the server to the local.
+        When folder synchronization is complete, you can click the [Download] button to store all the files locally.
       </p>
     </AccordionItem>
     <AccordionItem title="Delete folder">
       <p>
-        Click delete button if you want to delete folder & files on-chain.
+        Click the delete button if you want to delete folders and files on the server.
       </p>
     </AccordionItem>
   </Accordion>
